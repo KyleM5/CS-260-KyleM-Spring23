fc9394cd74ff72d49024dfa4fac9849225ff1143
KyleM5 <kyle.murray.3048@mail.linnbenton.edu>
2023-06-13 14:34:36 -0700

    Updated arbitrary_list.hpp
    Updated arbitrary list to layout the functions.

--- Final Project/arbitrary-list.hpp
+++ Final Project/arbitrary-list.hpp
@@ -22,6 +22,30 @@ class list {
         
             head = NULL; //Makes this start as an empty list
         };
+        
+        void insert(int value, int position) {
+            //Create the new node with the data we want to insert
+
+            //Counter for finding the spot used for inserting
+
+            //Test to make sure it is a valid insert position
+
+            //Now insert
+        };
+
+        void remove(int position) {
+            //Counter for finding the spot before deletion, store for later
+
+            //Find the next for thing we are deleting, store for later
+
+            //Make previous spot in list point further ahead and delete previous entry
+        };
+
+        void showData(int position) {
+            //Counter for finding data in the list
+
+            //return the data in that spot
+        };
 
 };
 
